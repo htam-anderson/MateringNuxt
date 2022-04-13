56c924a4474da975e39b301d7055db6d8301f119
Anderson Ho <htam.anderson@gmail.com>
2022-04-14 01:26:54 +0700

    2022-04-12/Lecture_4: d.Handling Errors

--- nuxtBnB/plugins/dataApi.js
+++ nuxtBnB/plugins/dataApi.js
@@ -2,19 +2,44 @@ export default function (context, inject) {
     const appId = '5ZJ56MSJNL'
     const apiKey = 'ede9a535f8efa3a360545281ab4c9008'
     const apiPathHomes = 'new-index-1649868776'
+    const headers = {
+        'X-Algolia-API-Key': apiKey,
+        'X-Algolia-Application-Id': appId,
+    }
 
     inject('dataApi', {
         getHome
     })
 
     async function getHome(homeId) {
-        const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/${apiPathHomes}/${homeId}`, {
-            headers: {
-                'X-Algolia-API-Key': apiKey,
-                'X-Algolia-Application-Id': appId,
-            }
-        })
+        try {
+            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/${apiPathHomes}/${homeId}`, { headers }))
+        } catch (error) {
+            return getErrorResponse(error)
+        }
+        
+    }
+
+    async function unWrap(response) {
         const json = await response.json()
-        return json
+        const { ok, status, statusText } = response
+
+        console.log(ok, status, statusText)
+
+        return {
+            json,
+            ok,
+            status,
+            statusText
+        }
+    }
+
+    function getErrorResponse(error) {
+        return {
+            of: false,
+            status: 500,
+            statusText: error.message,
+            json: {}
+        }
     }
 } 
